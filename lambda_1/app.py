3967e5074c99eeae61fd49b51d8d8dfd441cddc0
Kushagra <kushagracdhry.office@gmail.com>
2024-11-13 15:45:21 +0530

    changes in 1,2,3,4

--- lambda_1/app.py
+++ lambda_1/app.py
@@ -1,4 +1,4 @@
 def lambda_handler(event, context):
-    print("Hello from Lambda 1 latest") 
+    print("Hello from Lambda 1 latesthviehvie") 
     return {"statusCode": 200, "body": "Hello from Lambda 1"} 
 
--- lambda_3/app.py
+++ lambda_3/app.py
@@ -1,3 +1,3 @@
 def lambda_handler(event, context):
-    print("Hello from Lambda 3 latest") 
+    print("Hello from Lambda 3 latesthviehvie") 
     return {"statusCode": 200, "body": "Hello from Lambda 3"} 
