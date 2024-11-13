48dd26c09fab303a3deea4b716077d673dcadad8
Kushagra <kushagracdhry.office@gmail.com>
2024-11-13 17:59:45 +0530

    updated app.py and main.yml

--- lambda_1/app.py
+++ lambda_1/app.py
@@ -1,4 +1,4 @@
 def lambda_handler(event, context):
-    print("Hello from Lambda 1 latesthviehvie") 
-    return {"statusCode": 200, "body": "Hello from Lambda 1 Earth"} 
+    print("Hello from Lambda 1 new") 
+    return {"statusCode": 200, "body": "Hello from Lambda 1 New"} 
 
--- lambda_2/app.py
+++ lambda_2/app.py
@@ -1,3 +1,3 @@
 def lambda_handler(event, context):
     print("Hello from Lambda 2 latesthviehvie") 
-    return {"statusCode": 200, "body": "Hello from Lambda 2 updated"} 
+    return {"statusCode": 200, "body": "Hello from Lambda 2 New"} 
