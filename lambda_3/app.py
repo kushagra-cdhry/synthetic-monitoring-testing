02e35745d78645787057cb26bb5a49186e303b76
Kushagra <kushagracdhry.office@gmail.com>
2024-11-13 18:01:10 +0530

    updated app.py

--- lambda_3/app.py
+++ lambda_3/app.py
@@ -1,3 +1,3 @@
 def lambda_handler(event, context):
     print("Hello from Lambda 3 latesthviehvie") 
-    return {"statusCode": 200, "body": "Hello from Lambda 3 New"} 
+    return {"statusCode": 200, "body": "Hello from Lambda 3 Neww"} 
